864eef0c1bdebf241da3483668a856f45ed61a91
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2025-04-06 15:52:00 -0600

    add: info pages header
    se creo un arreglo para la información del header de las paginas

--- helpers/data.js
+++ helpers/data.js
@@ -40,3 +40,133 @@ export const ENLACES = [
     { id: 5, title: 'CFE', url: '/cfe' },
     { id: 6, title: 'Politica', url: '/politica' },
   ]
+
+
+export const TITLESPAGE = [
+    {
+        id: 1,
+        title: 'Registro Civil',
+        parrafo: 'Aquí encontrarás información sobre el registro civil en Doctor Mora, Guanajuato.',
+        url: '/registro-civil'
+    },
+    {
+        id: 2,
+        title: 'INE',
+        parrafo: 'Aquí encontrarás información sobre el INE en Doctor Mora, Guanajuato.',
+        url: '/ine'
+    },
+    {
+        id: 3,
+        title: 'Protección Civil',
+        parrafo: 'Aquí encontrarás información sobre protección civil en Doctor Mora, Guanajuato.',
+        url: '/proteccion-civil'
+    },
+    {
+        id: 4,
+        title: 'Seguridad Pública',
+        parrafo: 'Aquí encontrarás información sobre seguridad pública en Doctor Mora, Guanajuato.',
+        url: '/seguridad-publica'
+    },
+    {
+        id: 5,
+        title: 'CFE',
+        parrafo: 'Aquí encontrarás información sobre la CFE en Doctor Mora, Guanajuato.',
+        url: '/cfe'
+    },
+    {
+        id: 6,
+        title: 'Política',
+        parrafo: 'Aquí encontrarás información sobre política en Doctor Mora, Guanajuato.',
+        url: '/politica'
+    },
+    {
+        id: 7,
+        title: 'Lugares Turísticos',
+        parrafo: 'Descubre paisajes y tradiciones únicas en nuestros lugares turísticos. Sumérgete en la belleza natural y cultural de este destino.',
+        url: '/turismo'
+    },
+    {
+        id: 8,
+        title: 'Hoteles',
+        parrafo: 'Descubre hoteles con variedad de servicios, ideales para una estancia cómoda y adaptada a tus necesidades.',
+        url: '/hoteles'
+    },
+    {
+        id:9,
+        title: 'Estacionamientos',
+        parrafo: 'Aquí encontrarás información sobre estacionamientos en Doctor Mora, Guanajuato.',
+    },
+    {
+        id: 10,
+        title: 'Restaurantes',
+        parrafo: 'Descubre la variedad de restaurantes, donde podrás disfrutar de auténticos sabores locales y una rica tradición culinaria.',
+    },
+    {
+        id: 11,
+        title: 'Servicios',
+        parrafo: 'Aquí encontrarás información sobre servicios en Doctor Mora, Guanajuato.',
+
+    }
+]
+
+
+export const ESTACIONAMIENTOS = [
+    {
+        id: 1,
+        direccion: 'Calle Hidalgo #67'
+    },
+    {
+        id: 2,
+        direccion: 'Calle Hidalgo #48'
+    },
+    {
+        id: 3,
+        direccion: 'Calle Hidalgo #21'
+    },
+    {
+        id: 4,
+        direccion: 'Calle Irapuato #30'
+    },
+    {
+        id: 5,
+        direccion: 'Calle Irapuato #40'
+    },
+    {
+        id: 6,
+        direccion: 'Calle San Luis s/n'
+    },
+    {
+        id: 7,
+        direccion: 'Calle Guanajuato #10'
+    },
+    {
+        id: 8,
+        direccion: 'Calle Jerécuaro #10'
+    },
+    {
+        id: 9,
+        nombre: 'Estacionamiento Valtierra',
+        direccion: 'Calle San Luis #26',
+        url:'https://maps.app.goo.gl/ZN9r3Qgqrp6VnF1i9'
+    },
+    {
+        id: 10,
+        direccion: 'Calle Jaral #9'
+    },
+    {
+        id: 11,
+        direccion: 'Calle Abasolo #22',
+        url: 'https://www.google.com.mx/maps/place/21.1438056,-100.3179722'
+    },
+    {
+        id: 12,
+        direccion: 'Calle Jerécuaro #42',
+        url: 'https://maps.app.goo.gl/P2XHA9QNhZ8BaKSm9'
+    },
+    {
+        id: 13,
+        nombre: 'Estacionamiento La Huerta',
+        direccion: 'Calle Jerécuaro #52',
+        url: 'https://maps.app.goo.gl/MH75EZUv8miv2Gda8'
+    }
+]
