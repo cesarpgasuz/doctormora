01ae48ec54feb167c78f04879c2541017343690a
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2025-04-17 14:45:58 -0600

    update: tititulo paginas

--- components/ui/TitlesPages.jsx
+++ components/ui/TitlesPages.jsx
@@ -5,7 +5,7 @@ function TitlesPages(props) {
   return (
     <div className="text-center lg:w-1/2 lg:text-left">
           <span className="text-xs">Doctor Mora, Guanajuato</span>
-          <h1 className='text-4xl font-bold'>{props.title}</h1>
+          <h1 className='text-3xl sm:text-4xl font-bold'>{props.title}</h1>
           <p className='mt-2 mb-6 leading-7'>{props.parrafo}</p>
     </div>
   )
